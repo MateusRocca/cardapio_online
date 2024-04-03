f3c87fac71ca9d8ebad742ce75006fa8d7d75cb0
Mateus Rocca <galdinorocca@outlook.com>
2024-04-02 21:37:10 -0300

    Adicionando funcionalidades no carrinho de compras

---
+++ script.js
@@ -0,0 +1,57 @@
+const menu = document.querySelector('#menu');
+const cartBtn = document.querySelector('#cart-btn');
+const cartModal = document.querySelector('#cart-modal');
+const cartItensContainer = document.querySelector('#cart-items');
+const cartTotal = document.querySelector('#cart-total');
+const checkoutBtn = document.querySelector('#checkout-btn');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const cartCounter = document.querySelector('#cart-count');
+const addressInput = document.querySelector('#address');
+const addressWarn = document.querySelector('#address-warn');
+
+
+let cart = [];
+
+function abrirCarrinho() {
+    cartModal.style.display = 'flex';
+}
+
+function fecharCarrinho(){
+    cartModal.style.display = 'none';
+}
+
+//abrir carrinho de compras
+cartBtn.addEventListener('click', abrirCarrinho);
+
+//fechar o carrinho de compras clicando fora 
+cartModal.addEventListener('click', function(event) {
+    if(event.target === cartModal){
+        fecharCarrinho();
+    }
+})
+//fechar o carrinho de compras clicando no btn fechar
+closeModalBtn.addEventListener('click', fecharCarrinho);
+
+menu.addEventListener('click', function(event) {
+    let parentButton = event.target.closest('.add-to-cart-btn');
+    if(parentButton){
+        const name = parentButton.getAttribute('data-name');
+        const price = parentButton.getAttribute('data-price');
+        addToCart(name, price);
+    }
+})
+
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name);
+
+    if(existingItem){
+        existingItem.quantity += 1;
+        return;
+    }
+
+    cart.push({
+        name,
+        price,
+        quantity: 1,
+    });
+}
